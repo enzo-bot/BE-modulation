4a3271fdde5061b8a2bfd6f6771158506dfde00b
Slamnia Enzo <slamnia@insa-09151>
2023-11-24 16:46:39 +0100

    stereo + demod

--- manip3.py
+++ manip3.py
@@ -21,17 +21,15 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
-from PyQt5 import Qt
-from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import analog
 from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import filter
+from gnuradio.filter import firdes
 from gnuradio import gr
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
@@ -76,10 +74,11 @@ class manip3(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 1500000
+        self.samp_rate = samp_rate = 44100*7*5
         self.volume = volume = 1
+        self.panning = panning = 0.5
         self.freq_range = freq_range = 100e6
-        self.decimation = decimation = 6
+        self.decimation = decimation = 5
         self.cutoff_freq = cutoff_freq = 125e3
         self.bandwidth = bandwidth = samp_rate
 
@@ -106,51 +105,9 @@ class manip3(gr.top_block, Qt.QWidget):
         self.uhd_usrp_source_0.set_bandwidth(bandwidth, 0)
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
         self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec())
-        self.rational_resampler_xxx_0_0 = filter.rational_resampler_fff(
-                interpolation=3,
-                decimation=17,
-                taps=None,
-                fractional_bw=None)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-            1024, #size
-            firdes.WIN_BLACKMAN_hARRIS, #wintype
-            freq_range, #fc
-            bandwidth, #bw
-            "", #name
-            1
-        )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(True)
-
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self._panning_range = Range(0, 1, 0.05, 0.5, 200)
+        self._panning_win = RangeWidget(self._panning_range, self.set_panning, 'panning', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._panning_win)
         self.low_pass_filter_0 = filter.fir_filter_ccf(
             decimation,
             firdes.low_pass(
@@ -160,25 +117,23 @@ class manip3(gr.top_block, Qt.QWidget):
                 0.1*cutoff_freq,
                 firdes.WIN_HAMMING,
                 6.76))
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(volume)
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
-        self.audio_sink_0 = audio.sink(44100, '', True)
-        self.analog_wfm_rcv_0 = analog.wfm_rcv(
-        	quad_rate=samp_rate/decimation,
-        	audio_decimation=1,
-        )
+        self.blocks_multiply_const_vxx_0_0_0_1 = blocks.multiply_const_ff(volume)
+        self.audio_sink_0_1 = audio.sink(44100, '', True)
+        self.analog_nbfm_rx_0 = analog.nbfm_rx(
+        	audio_rate=44100,
+        	quad_rate=44100*7,
+        	tau=75e-6,
+        	max_dev=5e3,
+          )
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_rcv_0, 0), (self.blocks_float_to_complex_0, 0))
-        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0_0, 0))
-        self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
-        self.connect((self.rational_resampler_xxx_0_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.analog_nbfm_rx_0, 0), (self.blocks_multiply_const_vxx_0_0_0_1, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0_0_1, 0), (self.audio_sink_0_1, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_nbfm_rx_0, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.low_pass_filter_0, 0))
 
     def closeEvent(self, event):
@@ -200,14 +155,19 @@ class manip3(gr.top_block, Qt.QWidget):
 
     def set_volume(self, volume):
         self.volume = volume
-        self.blocks_multiply_const_vxx_0.set_k(self.volume)
+        self.blocks_multiply_const_vxx_0_0_0_1.set_k(self.volume)
+
+    def get_panning(self):
+        return self.panning
+
+    def set_panning(self, panning):
+        self.panning = panning
 
     def get_freq_range(self):
         return self.freq_range
 
     def set_freq_range(self, freq_range):
         self.freq_range = freq_range
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq_range, self.bandwidth)
         self.uhd_usrp_source_0.set_center_freq(self.freq_range, 0)
 
     def get_decimation(self):
@@ -228,7 +188,6 @@ class manip3(gr.top_block, Qt.QWidget):
 
     def set_bandwidth(self, bandwidth):
         self.bandwidth = bandwidth
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq_range, self.bandwidth)
         self.uhd_usrp_source_0.set_bandwidth(self.bandwidth, 0)
 
 
